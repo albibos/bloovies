01a0f15051c077279dbfcc8dc58765ead3b8c300
Albinus <99928204+albibos@users.noreply.github.com>
2023-04-11 15:00:09 -0400

    Update 7825439874412.js

--- 7825439874412.js
+++ 7825439874412.js
@@ -22,27 +22,30 @@ document.getElementById('content').style.display = 'none'
 }
   
 plyr.setup(video);
-} else {
-  const episodeId = urlParams.get("episodeId");
-  const mediaId = urlParams.get("mediaId");
-  
-  if (Hls.isSupported() && episodeId && mediaId) { // Check that episodeId and mediaId are not null
-    var hls = new Hls();
-    fetch(`https://api.consumet.org/movies/flixhq/watch?episodeId=${episodeId}&mediaId=${mediaId}`)
-      .then(response => response.json())
-      .then(data => {
-        hls.loadSource(data.sources[0].url);
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED,function() {
-          video.play();
-        });
-      });
-  }
-  
-  plyr.setup(video);
+}
+   
+if (urlParams.get("episode") {
+    document.getElementById('content').style.display = 'none'
+     var video = document.getElementById('player');
+     video.style.display = 'block';
+      const episodeId = urlParams.get("episode");
+      const mediaId = urlParams.get("show");
 
+      if (Hls.isSupported() && episodeId && mediaId) {
+        var hls = new Hls();
+        fetch(`https://api.consumet.org/movies/flixhq/watch?episodeId=${episodeId}&mediaId=${mediaId}`)
+          .then(response => response.json())
+          .then(data => {
+            hls.loadSource(data.sources[0].url);
+            hls.attachMedia(video);
+            hls.on(Hls.Events.MANIFEST_PARSED, function () {
+              video.play();
+            });
+          });
+      }
 }
 
+      plyr.setup(video);
 document.querySelector('#location').innerHTML = window.location.href + '?movie=watch-puss-in-boots-the-last-wish-91342';
 document.querySelector('#location').onclick = function() {
     window.open("/?movie=watch-puss-in-boots-the-last-wish-91342", "_blank");
